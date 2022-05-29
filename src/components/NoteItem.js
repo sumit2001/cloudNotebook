d25a4cfa77617635584ea91d6170599230f15ea3
sumit2001 <sgsumitgupta17@gmail.com>
2022-05-29 12:45:16 +0530

    added favicon

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -7,13 +7,15 @@ const NoteItem = (props) => {
     return (
         <div className="col-md-3">
             <div className="card my-3">
+                <div className="icons" style={{ position: "absolute", right: 0 }}>
+                    <i className="fa-solid fa-trash mx-2" onClick={() => {
+                        deleteNote(note._id); props.showAlert("Deleted Successfully", "success");
+                    }}></i>
+                    <i className="fa-solid fa-pen-to-square mx-2" onClick={() => { updateNote(note) }}></i>
+                </div>
                 <div className="card-body">
                     <div className="d-flex align-items-center">
-                        <h5 className="card-title">{note.title}</h5>
-                        <i className="fa-solid fa-trash mx-2" onClick={() => {
-                            deleteNote(note._id); props.showAlert("Deleted Successfully", "success");
-                        }}></i>
-                        <i className="fa-solid fa-pen-to-square mx-2" onClick={() => { updateNote(note) }}></i>
+                        <h5 className="card-title">{note.title.length > 26 ? note.title.slice(0, 25) + "..." : note.title}</h5>
                     </div>
                     <p className="card-text">{note.description}</p>
                 </div>
